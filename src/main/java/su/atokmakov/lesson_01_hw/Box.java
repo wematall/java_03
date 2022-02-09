63e1bf8a8b852adccf287978ffd6e9f917c59f6e
Andrey T <wematall@gamil.com>
2022-02-09 21:22:18 +0300

    HW 1 Task 3 part c

--- src/main/java/su/atokmakov/lesson_01_hw/Box.java
+++ src/main/java/su/atokmakov/lesson_01_hw/Box.java
@@ -1,5 +1,22 @@
 package su.atokmakov.lesson_01_hw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Box <T extends Fruit> {
-    
+    private List<T> list;
+
+    public Box() {
+        list = new ArrayList<T>();
+    }
+
+    public Box(T... obj) {
+        list = Arrays.asList(obj);
+    }
+
+
+    public List<T> getList() {
+        return list;
+    }
 }
