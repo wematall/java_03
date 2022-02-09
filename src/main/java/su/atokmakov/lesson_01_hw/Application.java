7e636ffbc5dbba1278907b6d1d7a1e100203464e
Andrey T <wematall@gamil.com>
2022-02-09 20:35:11 +0300

    HW 1 Task 2

--- src/main/java/su/atokmakov/lesson_01_hw/Application.java
+++ src/main/java/su/atokmakov/lesson_01_hw/Application.java
@@ -1,6 +1,7 @@
 package su.atokmakov.lesson_01_hw;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,6 +18,17 @@ public class Application {
         System.out.println(Arrays.toString(myArr));
 
 
+        // Задание 2
+        // Написать метод, который преобразует массив в ArrayList;
+        System.out.println();
+        System.out.println("Task 2");
+        System.out.println("Array before conversion");
+        System.out.println(myArr.getClass());
+        List<String> list = convertArrayToArrayList(myArr);
+        System.out.println("After conversion");
+        System.out.println(list.getClass());
+
+
 
     }
 
@@ -28,5 +40,14 @@ public class Application {
         arr[index1] = arr[index2];
         arr[index2] = tmp;
 
+    }
+
+    // Задание 2
+    // Написать метод, который преобразует массив в ArrayList;
+
+    public static <T> List<T> convertArrayToArrayList(T[] arr) {
+        List<T> list = Arrays.asList(arr);
+
+        return list;
     }
 }
